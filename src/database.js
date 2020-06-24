89444c3414bb39527ae34c0012a7f053b4a5ffdd
Gabriel Valero <ninjagabo131280@gmail.com>
2020-06-24 16:51:18 -0400

    arreglos

--- src/database.js
+++ src/database.js
@@ -1,13 +1,14 @@
 
 const mongoose = require('mongoose');
 
-const URL =process.env.MONGODB_URL || 'mongodb+srv://ninjagabo:Ninjagabo.1312@cluster0-ni87o.mongodb.net/Cluster0?retryWrites=true&w=majority';
+const URL = process.env.MONGODB_URL;
 
 
 mongoose.connect(URL,{
 	useNewUrlParser: true,
+	useCreateIndex: true,
 	useFindAndModify: false,
 	useUnifiedTopology: true 
 })
-.then(db => console.log('DB is connected ' + process.env.MONGODB_URL))
+.then(db => console.log('DB is connected '))
 .catch(err=> console.log('Algo paso ', err))
